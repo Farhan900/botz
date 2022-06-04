5b9e7d711a6c966fc7353921df721e819ed3668c
Jayaganz26 <106452810+Jayaganz26@users.noreply.github.com>
2022-06-05 07:39:09 +0800

    Update _antitroli.js

--- plugins/_antitroli.js
+++ plugins/_antitroli.js
@@ -1,11 +1,7 @@
-export default {
-    async all(m) {
+export async function async all (m) {
         if (m.message && m.isBaileys && m.quoted && m.quoted.mtype === 'orderMessage' && !(m.quoted.token && m.quoted.orderId)) {
             m.reply('Bug Troli Detected\n\n' + require('util').format(m.key))
             // await this.clearMessage(m.chat, m.key)
-            await this.modifyChat(m.chat, 'clear', {
-                includeStarred: false
-            }).catch(console.log)
-        }
-    }
+            await this.modifyChat(m.chat, 'clear', { includeStarred: false }).catch(console.log)
+       }
 }
