daf453c9bfe47326ffcde030e0b2f9049c493813
Jayaganz26 <106452810+Jayaganz26@users.noreply.github.com>
2022-06-05 08:35:54 +0800

    Add files via upload

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,14 +1,13 @@
-import { igdl } from '../lib/scrape.js'
+import fetch from 'node-fetch'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) throw `Pengunaan:\n${usedPrefix + command} <url>\n\nContoh:\n${usedPrefix + command} https://www.instagram.com/p/CQU21b0JKwq/`
-  if (!args[0].match(/https:\/\/www.instagram.com\/.*(p|reel|tv)/gi)) throw `Url salah, perintah ini untuk mengunduh post/reel/tv`
-  igdl(args[0]).then(async res => {
-    let igdl = JSON.stringify(res)
-    let json = JSON.parse(igdl)
-    for (let { downloadUrl, type } of json) {
-      conn.sendFile(m.chat, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), author, m)
-    }
-  })
+  if (!args[0]) throw `Masukan url!`
+  if(/(?:\/p\/|\/reel\/|\/tv\/)([^\s&]+)/i.test(args[0])) {
+  let anu = await fetch(global.API('zenz', '/downloader/instagram2', { url: args[0] }, 'apikey'))
+  for (let media of anu.data) conn.sendFile(m.chat, media, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), author, m)
+  } else if(/\/stories\/([^\s&]+)/i.test(args[0])) {
+  let anu = await fetch(global.API('zenz', '/downloader/instastory', { url: args[0] }, 'apikey'))
+  conn.sendFile(m.chat, anu.media[0].url, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), author, m)
+  }
 }
 handler.help = ['ig'].map(v => v + ' <url>')
 handler.tags = ['downloader']
