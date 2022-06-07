6f6909b86f3f3073fe766571ab32b8452ebfeb82
Jayaganz26 <106452810+Jayaganz26@users.noreply.github.com>
2022-06-07 13:50:52 +0800

    Update game-tebakbendera_hint.js

--- plugins/game-tebakbendera_hint.js
+++ plugins/game-tebakbendera_hint.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn }) => {
     let clue = ans.replace(/[bcdfghjklmnpqrstvwxyz]/ig, '_')
     m.reply('```' + clue + '```')
 }
-handler.command = /^hint$/i
+handler.command = /^teba$/i
 
 handler.limit = true
 
