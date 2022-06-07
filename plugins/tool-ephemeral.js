930e34d0ec50321769b45333b23ebf3469acb6af
Jayaganz26 <106452810+Jayaganz26@users.noreply.github.com>
2022-06-07 19:40:09 +0800

    Update tool-ephemeral.js

--- plugins/tool-ephemeral.js
+++ plugins/tool-ephemeral.js
@@ -5,6 +5,8 @@ let handler = async (m, { conn, args, isBotAdmin, isAdmin, isOwner }) => {
         if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
     }
     if (args[0] == 'on') await conn.sendMessage(m.chat, { disappearingMessagesInChat: WA_DEFAULT_EPHEMERAL })
+    else if (args[0] == '24') await conn.sendMessage(m.chat, { disappearingMessagesInChat: 24 * 60 * 60 })
+    else if (args[0] == '9') await conn.sendMessage(m.chat, { disappearingMessagesInChat: 90 * 60 * 60 })
     else if (args[0] == 'off') await conn.sendMessage(m.chat, { disappearingMessagesInChat: false })
 }
 handler.help = ['ephe [on]']
