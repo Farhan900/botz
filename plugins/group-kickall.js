94510d7903aa4dd0bf436bc2111d7755a2a63a52
Jayaganz26 <106452810+Jayaganz26@users.noreply.github.com>
2022-06-07 13:57:45 +0800

    Create group-kickall.js

---
+++ plugins/group-kickall.js
@@ -0,0 +1,22 @@
+let handler = async (m, { conn }) => {
+  const delay = time => new Promise(res=>setTimeout(res,time));
+  let ownerGroup = m.chat.split`-`[0] + '@s.whatsapp.net'
+  let users = (await conn.fetchGroupMetadataFromWA(m.chat)).participants.map(u => u.jid)
+  for (let user of users){
+    if (user !== ownerGroup + '@s.whatsapp.net' && user !== global.conn.user.jid && user !== global.owner + '@s.whatsapp.net'){
+      await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
+      await delay(3000)
+    }
+  }
+}
+handler.help = ['kickall']
+handler.tags = ['admin']
+handler.command = /^(kickall)$/i
+handler.owner = false
+handler.group = true
+handler.fail = null
+
+handler.botAdmin = true
+handler.admin = true
+
+export default handler
