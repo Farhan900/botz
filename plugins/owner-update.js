3d1a4232eebef5b5ea48f2d8c41c9332381709f5
jayabotz <107167904+jayabotz@users.noreply.github.com>
2022-06-12 17:35:19 +0800

    Update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,7 +1,7 @@
 import { execSync } from 'child_process'
 let handler = async (m, { conn, text, isROwner }) => {
   if (global.conn.user.jid == conn.user.jid) {
-    let stdout = execSync('git remote set-url origin https://github.com/Nurutomo/wabot-aq.git && git pull' + (isROwner && text ? ' ' + text : ''))
+    let stdout = execSync('git remote set-url origin https://github.com/Jayaganz26/wa-bot.git && git pull' + (isROwner && text ? ' ' + text : ''))
     if (isROwner) import 'fs'.readdirSync('plugins').map(v => global.reload('', v))
     m.reply(stdout.toString())
   }
